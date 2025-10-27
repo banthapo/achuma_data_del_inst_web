da5d59879b8a383b3d91a7075224b0f28b39322e
banthapo <lchijuwa@gmail.com>
2025-10-28 01:50:24 +0200

    Achuma data deletion instruction web

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <head>
+        <title>Achuma Data Deletion Instructions</title>
+      </head>
+      <body>
+        <h2>Data Deletion Request</h2>
+        <p>
+          If you would like to delete your data associated with Achuma
+          application, please follow these steps:
+        </p>
+        <ol>
+          <li>Exit all joined or created groups on achuma application.</li>
+          <li>Select unregistering from the apps drawer menu.</li>
+          <li>
+            Confirm the process and your data will be removed apon
+            unregistering.
+          </li>
+        </ol>
+        <p>
+          Alternatively, you can remove your app data directly from your
+          Facebook account settings:
+          <a href="https://www.facebook.com/settings?tab=applications">
+            https://www.facebook.com/settings?tab=applications
+          </a>
+        </p>
+      </body>
+    </>
+  );
+}
+
+export default App;
